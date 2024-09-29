1a5be87f37617bd6b394b823ef8efd4cf8c09e22
Luca_Amorosi <amorosi.luca1@gmail.com>
2024-09-29 04:56:35 +0200

    [mesh] ADD.

---
+++ python/sticky_controller/core/mesh.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+from maya import cmds
+from maya.api import OpenMaya as om
+
+from sticky_controller import utils
+
+
+def get_shape_deformed(transform: str, create: bool = True) -> str | None:
+    """For given transform, get its shape deformed. A shapeDeformed is
+    considered as a shape without intermediateObject checked.
+
+    :param transform: Transform to get shapeDeformed from.
+    :param create: If a shapeDeformed should be created if not found.
+
+    :returns: The deformed shape DagNode or None.
+    """
+    shapes = transform.get_shapes(path=True)
+    if len(shapes) != 1:
+        for shape in shapes:
+            if not shape["intermediateObject"].get():
+                return shape
+
+    return create_shape_deformed(transform) if create else None
+
+
+def create_shape_deformed(transform: str) -> str:
+    """Create an orig shape and return deformedShape for non referenced
+    deformable object. Or create a shapeDeformed and returns it for referenced
+    deformable object.
+
+    :returns DagNode: Shape deformed.
+    """
+    original_shape = get_original_shape(transform)
+
+    if transform.is_referenced:
+        shape_deformed = f"{transform.name.split(':')[-1]}ShapeDeformed"
+        shape_deformed = transform.duplicate_shape(name=shape_deformed)
+    else:
+        shape_deformed = original_shape
+        original_shape = transform.duplicate_shape(name=f"{transform}ShapeOrig")
+
+    original_shape["intermediateObject"].set(True)
+
+    return shape_deformed
+
+
+def get_original_shape(transform: str) -> str:
+    """Returns the original shape of a transform. The original Shape is
+    considered as the referenced shape for referenced geometries. Or for
+    non-referenced geometries, as a shape without any input connection in its
+    input attribute. If multiple shapes fulfill this condition get the shape
+    which endswith "Orig".
+
+    :param transform: Transform DagNode.
+
+    :returns: DagNode of shape or None if no original shape found.
+
+    :raises RuntimeError: If no shapes found.
+    """
+    shapes = cmds.listRelatives(transform, shapes=True, path=True)
+    if not shapes:
+        raise RuntimeError(f"Node -{transform}- doesn't have any shape !")
+
+    if transform.is_referenced:
+        for shape in shapes:
+            if shape.is_referenced:  # Referenced shape is the original shape.
+                return shape
+
+    # For non referenced geometries.
+    orig_shapes = []
+    for shape in shapes:
+        if not shape["inMesh"].inputs():
+            orig_shapes.append(shape)
+
+    if len(orig_shapes) == 1:
+        return orig_shapes[0]
+
+    for shape in orig_shapes:
+        if shape.name.endswith("Orig"):
+            return shape
+
+
+def get_deformers(
+    mesh: str, return_name: bool = False, deformer_types: list[str] = None
+) -> list[str] | list[om.MObject]:
+    """Get all the deformers ordered connected the mesh.
+
+    :param mesh: Mesh to get deformers from, must be a fullPathName.
+    :param return_name: Returns names of deformers' name or their MObject.
+    :param deformer_types: Only returns deformer of these types.
+
+    :returns: Empty list, or list[str] or list[MObject]
+    """
+    deformer_types = deformer_types or []
+    common_types = 1 if not deformer_types else 2
+    deformer_types.insert(0, "geometryFilter")
+
+    deformers = []
+    for node in cmds.listHistory(mesh):
+        node_types = cmds.nodeType(node, inherited=True)
+        if not utils.has_common_members(
+            node_types, deformer_types, common_types
+        ):
+            # Not of type "geometryFilter" and any of given deformer_types.
+            continue
+
+        # Wrap deformer has no originalGeo attribute, we then get geoMatrix.
+        orig_attr = "geomMatrix" if "wrap" in node_types else "originalGeometry"
+        orig_geos = cmds.listConnections(
+            f"{node}.{orig_attr}", source=True, shapes=True
+        )
+        if not orig_geos:
+            # if no input connections in originalGeo or geomMatrix.
+            continue
+        shapes = cmds.listRelatives(mesh, shapes=True, path=True)
+        if not utils.has_common_members(orig_geos, shapes):
+            # Check if any shapes are in originalGeos. This way we can get the
+            # deformer even if there are duplicated deformed shapes.
+            continue
+
+        node = node if return_name else maya_api.to_mobject(node)
+        if node not in deformers:
+            deformers.append(node)
+
+    return deformers
